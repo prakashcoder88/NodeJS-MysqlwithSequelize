c96a6ed6d453ef7f7767115ce080a8f5d223851b
PrakashPatel <patelprakash21@gmail.com>
2023-10-13 12:37:21 +0530

    update

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
 const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+
+require("dotenv").config()
+const PORT = process.env.SERVERPORT || 6000
+
+
 const app = express()
+
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
+
+
+app.listen(PORT, () =>{
+    console.log(`Successfully running port on ${PORT}`);
+})
